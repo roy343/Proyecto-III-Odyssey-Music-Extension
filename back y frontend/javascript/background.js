22b7ebdc6815b95a5059bec687e2b77e37950f23
Marco <marco99eu@gmail.com>
2021-01-18 15:02:30 -0600

    Added back and front end

---
+++ back y frontend/javascript/background.js
@@ -0,0 +1,103 @@
+/**
+ * Creates a text for suggestions on omnibox
+ * @param {object} data received object from server
+ */
+function suggestionListAux(data) {
+    let songStr = "Song: " + data.track_name;
+    let artistStr = "     /     Artist: " + data.artist;
+    return songStr + artistStr;
+}
+
+/**
+ * Returns the omnibox's suggestions (a list of objects)
+ * @param {*} data information received from the request
+ * @param {number} amount ammount of suggestions that will be displayed
+ */
+function suggestionList(data, amount) {
+    var list = [];
+    for (i = 0; i < amount; i++) {
+        var object = { content: data[i].id, description: suggestionListAux(data[i]) };
+        list.push(object);
+    }
+    suggestions = list;
+    return list;
+}
+
+const TIME_BETWEEN_REQUESTS = 500;
+const AMOUNT_OF_SUGGESTIONS = 5;
+const ALL_SUGGESTIONS = 9;
+
+var songList = []
+var currentSong = 0;
+var hasAllKeyword = false;
+var lastRequestTime = -TIME_BETWEEN_REQUESTS;
+var actualRequestTime;
+
+chrome.omnibox.setDefaultSuggestion({ description: "Search a song by its name, artist or lyrics" });
+
+// Omnibox listener, used everytime the omnibox's text is updated
+chrome.omnibox.onInputChanged.addListener(
+    function(text, suggest) {
+
+        // Constructing the get request text
+        actualRequestTime = new Date().getTime();
+        if (text == "*") {
+            text = "";
+        }
+        var apiCall;
+        if (text.length > 30) {
+            apiCall = 'https://localhost:3000/songs' // Search by lyric
+        } else {
+            apiCall = 'https://localhost:3000/songs' // Search by user input
+        }
+        if (actualRequestTime - lastRequestTime > TIME_BETWEEN_REQUESTS) {
+            lastRequestTime = actualRequestTime;
+            fetch(apiCall).then(function(res) {
+                // If the server is down
+                if (res.status !== 200) {
+                    suggest([
+                        { content: "None", description: "Something went wrong" }
+                    ])
+                }
+                // Add the sugestions given by the server
+                res.json().then(function(data) {
+                    if (text == "") {
+                        list = suggestionList(data, ALL_SUGGESTIONS);
+                    } else {
+                        list = suggestionList(data, AMOUNT_OF_SUGGESTIONS);
+                    }
+                    suggest(list);
+                }).catch(function(err) {
+                    suggest([{ content: 'Error', description: 'There was a problem loading the server' }]);
+                });
+            })
+        } else {
+            suggest(suggestions);
+        }
+    });
+
+// This event is fired with the user accepts the input in the omnibox.
+chrome.omnibox.onInputEntered.addListener(
+    function(text) {
+        songList.push('https://open.spotify.com/embed/track/' + text);
+
+    });
+
+chrome.runtime.onMessage.addListener((msg, sender, response) => {
+    if (songList.length == 0) {
+        console.log("Empty list");
+        response(["Empty List", currentSong + 1, songList.length]);
+    } else {
+        if (msg.name == "Next Song" & currentSong + 1 < songList.length) {
+            currentSong += 1;
+            response([songList[currentSong], currentSong + 1, songList.length]);
+        } else if (msg.name == "Previous Song" & currentSong !== 0) {
+            currentSong -= 1;
+            response([songList[currentSong], currentSong + 1, songList.length]);
+        } else if (msg.name == "Current Song") {
+            response([songList[currentSong], currentSong + 1, songList.length]);
+        } else {
+            response(["No Movement", currentSong + 1, songList.length]);
+        }
+    }
+});
---
+++ back y frontend/javascript/script.js
@@ -0,0 +1,48 @@
+/**
+ * Enables or disables next and previous song buttons
+ * @param {Array} response 
+ */
+function enableButtons(response) {
+    if (response[1] == response[2]) {
+        document.getElementById('nextButton').disabled = true;
+    } else {
+        document.getElementById('nextButton').disabled = false;
+    }
+    if (response[1] == 1) {
+        document.getElementById('previousButton').disabled = true;
+    } else {
+        document.getElementById('previousButton').disabled = false;
+    }
+}
+
+/**
+ * Sends message to background to get the next or previous song in the playlist
+ * @param {string} song 
+ */
+function playSong(song) {
+    chrome.runtime.sendMessage({ name: song }, (response) => {
+        enableButtons(response);
+        if (response[0] == "Empty List") {
+            document.getElementById('nextButton').disabled = true;
+            document.getElementById('previousButton').disabled = true;
+            document.getElementById('playlistCounter').innerHTML = "0/0"
+        } else if (response[0] == "No Movement") {
+            document.getElementById('playlistCounter').innerHTML = response[1] + '/' + response[2];
+        } else {
+            document.getElementById('iframePlayer').src = response[0];
+            document.getElementById('playlistCounter').innerHTML = response[1] + '/' + response[2];
+        }
+    });
+}
+
+function playNextAux() {
+    playSong("Next Song");
+}
+
+function playPreviousAux() {
+    playSong("Previous Song")
+}
+
+document.getElementById('nextButton').addEventListener('click', playNextAux);
+document.getElementById('previousButton').addEventListener('click', playPreviousAux);
+playSong("Current Song");
